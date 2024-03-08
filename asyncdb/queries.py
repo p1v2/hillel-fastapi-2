73e54789aa8e33a7a5980b574df370e95640ea30
foom1r <foom1rrar@gmail.com>
2024-03-08 19:49:53 +0300

    dz

--- asyncdb/queries.py
+++ asyncdb/queries.py
@@ -11,3 +11,13 @@ async def get_products(db: AsyncSession, offset: int, limit: int):
         products = result.scalars().all()
 
         return products
+
+
+async def product_by_id(db: AsyncSession, product_id: int):
+    async with db as session:
+        result = await session.execute(
+            select(ProductModel)
+            .where(ProductModel.id == product_id)
+        )
+        return result.scalars().first()
+    
